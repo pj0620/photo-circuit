11aeba425e806cd93a6ef45e01dc9295116403c9
pj0620 <pjm7748@g.rit.edu>
2024-07-05 23:18:33 -0400

    add langchain

---
+++ photo-circuit-api/src/main.rs
@@ -0,0 +1,92 @@
+use std::error::Error;
+
+use langchain_rust::{
+  chain::{Chain, LLMChainBuilder},
+  fmt_message, fmt_placeholder, fmt_template,
+  language_models::llm::LLM,
+  llm::openai::{OpenAI, OpenAIModel},
+  message_formatter,
+  prompt::HumanMessagePromptTemplate,
+  prompt_args,
+  schemas::messages::Message,
+  template_fstring,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+  dotenv::dotenv().ok();
+  let open_ai = OpenAI::default().with_model(OpenAIModel::Gpt35.to_string());
+
+
+  //Once you've installed and initialized the LLM of your choice, we can try using it! Let's ask it what LangSmith is - this is something that wasn't present in the training data so it shouldn't have a very good response.
+  let resp = open_ai.invoke("What is rust").await.unwrap();
+  println!("{}", resp);
+
+  // We can also guide it's response with a prompt template. Prompt templates are used to convert raw user input to a better input to the LLM.
+  let prompt = message_formatter![
+      fmt_message!(Message::new_system_message(
+          "You are world class technical documentation writer."
+      )),
+      fmt_template!(HumanMessagePromptTemplate::new(template_fstring!(
+          "{input}", "input"
+      )))
+  ];
+
+  //We can now combine these into a simple LLM chain:
+
+  let chain = LLMChainBuilder::new()
+      .prompt(prompt)
+      .llm(open_ai.clone())
+      .build()
+      .unwrap();
+
+  //We can now invoke it and ask the same question. It still won't know the answer, but it should respond in a more proper tone for a technical writer!
+
+  match chain
+      .invoke(prompt_args! {
+      "input" => "Quien es el escritor de 20000 millas de viaje submarino",
+         })
+      .await
+  {
+      Ok(result) => {
+          println!("Result: {:?}", result);
+      }
+      Err(e) => panic!("Error invoking LLMChain: {:?}", e),
+  }
+
+  //If you want to prompt to have a list of messages you could use the `fmt_placeholder` macro
+
+  let prompt = message_formatter![
+      fmt_message!(Message::new_system_message(
+          "You are world class technical documentation writer."
+      )),
+      fmt_placeholder!("history"),
+      fmt_template!(HumanMessagePromptTemplate::new(template_fstring!(
+          "{input}", "input"
+      ))),
+  ];
+
+  let chain = LLMChainBuilder::new()
+      .prompt(prompt)
+      .llm(open_ai)
+      .build()
+      .unwrap();
+  match chain
+      .invoke(prompt_args! {
+      "input" => "Who is the writer of 20,000 Leagues Under the Sea, and what is my name?",
+      "history" => vec![
+              Message::new_human_message("My name is: luis"),
+              Message::new_ai_message("Hi luis"),
+              ],
+
+      })
+      .await
+  {
+      Ok(result) => {
+          println!("Result: {:?}", result);
+      }
+      Err(e) => panic!("Error invoking LLMChain: {:?}", e),
+  }
+
+  Ok(())
+}
